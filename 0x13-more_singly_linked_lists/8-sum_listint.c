1c3272ea78c8062d0c3296d874117cf5355aabeb
CasShandu <cas.shandu@gmail.com>
2023-11-21 04:01:45 -0800

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum up all data of a list
+ * @head: pointr to the list
+ * Return: sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+
+	}
+	return (sum);
+
+}
